745fdc98afef24518e52f696f53e63dba609b65f
Marina Berdnikova <berdni4ka@rambler.ru>
2023-04-11 00:59:08 +0300

    Add task 5

---
+++ src/Task_5.java
@@ -0,0 +1,41 @@
+import java.util.ArrayDeque;
+import java.util.Stack;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку, содержащую только символы '(', ')', '{', '}', '[' и ']'");
+        String string = scanner.nextLine();
+        if (validate(string)){
+            System.out.println("Строка логически правильная.");
+        }
+        else{
+            System.out.println("Строка логически неправильная.");
+        }
+        
+    }
+    
+    public static boolean validate(String string){
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < string.length(); i++) {
+            char symbol = string.charAt(i);
+            if (symbol == '(' || symbol == '[' || symbol == '{') {
+                stack.push(symbol);
+            } else if (symbol == ')' || symbol == ']' || symbol == '}') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char last = stack.peek();
+                if ((symbol == ')' && last == '(') || (symbol == ']' && last == '[') || (symbol == '}' && last == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+        
+    }
+}
